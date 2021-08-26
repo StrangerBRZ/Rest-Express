557fe7e8ec83b6eb7d48dbc60b2002404b3ad963
aznzwq <gavinzheng.utoronto@hotmail.com>
2021-08-26 04:53:04 -0400

    Added code for Heroku app
    Added code for Heroku app

--- server/index.js
+++ server/index.js
@@ -13,11 +13,17 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.json({ message: "Hello from server!" })
 })
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
 
 app.use('/api', taskRouter)
 
