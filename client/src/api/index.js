6ae438697f443ca7899c95a99c51d0e73e14a969
aznzwq <gavinzheng.utoronto@hotmail.com>
2021-08-26 11:38:04 -0400

    changed client link in api
    changed client link in api to match heroku url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://rest-express-app.herokuapp.com/api',
 })
 
 export const insertTask = payload => api.post(`/task`, payload)
